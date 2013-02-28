d895efe1b4c7cca995c546709ef40e12a14f6bcd
Ali <aliasgar@outlook.com>
2013-02-28 15:42:17 +0530

    Updated previous coordinates php file

--- php/show.php
+++ php/show.php
@@ -1,6 +1,10 @@
 <?php
 	include_once("config.inc");
-	$query = "select * from bus_log;";
+	$lim=$_GET["limit"];
+	$table=$_GET["table"];
+	if($table != "balance") $table="bus_log";
+	if($lim<20) $lim=20;
+	$query = "select * from $table order by time,id desc limit $lim;";
 	$result = mysql_query($query);
 	$rows=array();
 	while($row = mysql_fetch_assoc($result)) {
