1c36800db0a47dbde6d53121bb56d939e1fad467
Ali <aliasgar@outlook.com>
2013-02-28 10:59:41 +0530

    Updated previous coordinates php file

--- php/get-prev-coord.php
+++ php/get-prev-coord.php
@@ -1,9 +1,8 @@
 <?php
 	include_once("config.inc");
 	$busid = $_GET["id"];
-	$time = date('Y/m/d H:i:s');
-	$query = "select * from bus_log where bus_id = $busid and time = '$time' order by time desc;";
-	echo $query;
+	$time = date('Y/m/d');
+	$query = "select * from bus_log where bus_id = $busid and time = '$time%' order by time desc;";
 	$result = mysql_query($query);
 	$rows= array();
 	while($row = mysql_fetch_assoc($result)) {
