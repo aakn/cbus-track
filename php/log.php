e3e604d7d60f3440a7e35951fa16ab800b163c6f
Ali <aliasgar@outlook.com>
2013-03-13 20:22:59 +0530

    Debugging deploy curl code

--- php/log.php
+++ php/log.php
@@ -53,9 +53,9 @@
 	$base_url = 'track.pesseacm.org';
 	if ($balance == "") $balance = "BALANCEFAIL";
 	//$ch = curl_init("django.insigniadevs.com/add/1/1253.8802N/07735.3015E/50/A/e$is$45.43$INR.Valid/");
-	$url = "$base_url/add/$bus_id/$oldlat/$oldlon/$oldspeed/$v/$balance/";
+	$url = "$base_url/add/$bus_id/$oldlat/$oldlon/$oldspeed/$v/".url_encode($balance)."/";
 	// echo "<br/>$url<br/>";
-	$ch = curl_init(url_encode($url));
+	$ch = curl_init($url);
 	curl_exec($ch);
 	curl_close($ch);
 
