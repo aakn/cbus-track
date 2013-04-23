6fbd8d30e6b59c8a7a85a8bc8b79905348365161
Ali <aliasgar@outlook.com>
2013-04-24 00:13:55 +0530

    Changed request to head

--- php/log.php
+++ php/log.php
@@ -51,7 +51,7 @@
 		 );
 	// $pusher->trigger('track-channel', 'bus-moved', $array );
 	$base_url = 'track.pesseacm.org';
-	if ($balance == "") $balance = "BALANCEFAIL";
+	if (IsNullOrEmptyString($balance)) $balance = "BALANCEFAIL";
 	//$ch = curl_init("django.insigniadevs.com/add/1/1253.8802N/07735.3015E/50/A/e$is$45.43$INR.Valid/");
 	$url = "$base_url/add/$bus_id/$oldlat/$oldlon/$oldspeed/$v/".urlencode($balance)."/";
 	// echo "<br/>$url<br/>";
@@ -81,4 +81,9 @@
 
 	    return $deg+( (($min*60) + ($sec)) /3600);
 	} 
+
+	// Function for basic field validation (present and neither empty nor only white space
+	function IsNullOrEmptyString($question){
+		return (!isset($question) || trim($question)==='');
+	}
 ?>
